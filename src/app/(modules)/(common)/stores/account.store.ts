10733697c9d25bd7af3641fcb41b664992809afc
jhonatanmizu <natanjesuss20@gmail.com>
2024-11-19 21:50:01 -0300

    :sparkles: feat: create account and payment store

---
+++ src/app/(modules)/(common)/stores/account.store.ts
@@ -0,0 +1,74 @@
+import { create } from "zustand";
+import { IAccount, ICard } from "@/src/app/(modules)/(common)/types";
+
+const mockedAccount: IAccount = {
+  accountId: "123456789",
+  balance: 2000.0,
+  currency: "BRL",
+  status: "active",
+  owner: { name: "John Doe", id: "987654321" },
+  cards: [
+    {
+      cardId: "1",
+      name: "Platinum",
+      securityCode: "***",
+      cardNumber: "****181",
+      holder: "John Doer",
+      expirationDate: "12/2025",
+      brand: "Visa",
+      favorite: true,
+      used: true,
+    },
+  ],
+};
+
+interface AccountStoreState {
+  account: IAccount;
+  isLoadingAccount: boolean;
+  isLoadingCards: boolean;
+}
+
+interface AccountStoreAction {
+  getAccount: () => Promise<IAccount>;
+  getCards: () => Promise<ICard[]>;
+}
+
+const initialState: AccountStoreState = {
+  account: mockedAccount,
+  isLoadingAccount: false,
+  isLoadingCards: false,
+};
+
+export const useAccountStore = create<AccountStoreState & AccountStoreAction>(
+  (set, get) => ({
+    ...initialState,
+    getCards: async () => {
+      set({ isLoadingCards: true });
+      try {
+        const result = await new Promise<ICard[]>((resolve) =>
+          setTimeout(() => resolve(get().account.cards), 5000)
+        );
+        return result;
+      } catch (error) {
+        console.error("Error when trying to get cards", error);
+        throw error;
+      } finally {
+        set({ isLoadingCards: false });
+      }
+    },
+    getAccount: async () => {
+      set({ isLoadingAccount: true });
+      try {
+        const result = await new Promise<IAccount>((resolve) =>
+          setTimeout(() => resolve(get().account), 5000)
+        );
+        return result;
+      } catch (error) {
+        console.error("Error when trying to get account", error);
+        throw error;
+      } finally {
+        set({ isLoadingAccount: false });
+      }
+    },
+  })
+);
---
+++ src/app/(modules)/(common)/stores/payment.store.ts
@@ -0,0 +1,75 @@
+import { create } from "zustand";
+import { IPayment, ISimulation } from "@/src/app/(modules)/(common)/types";
+
+const mockedPayment: IPayment = {
+  transactionId: "abc123",
+  amount: 100.0,
+  currency: "BRL",
+  receiver: {
+    name: "Maria da Silca",
+    id: "45648941",
+  },
+  method: "credit_card",
+  simulation: [
+    {
+      amountToPay: 103.0,
+      installmentAmount: 103.0,
+      installments: 1,
+      fees: {
+        fixed: { amount: 3.0, percentage: 0.03 },
+        installments: { amount: 0.0, percentage: 0.01 },
+      },
+    },
+  ],
+};
+
+interface PaymentStoreState {
+  payment: IPayment;
+  isLoadingPayment: boolean;
+  isLoadingSimulations: boolean;
+}
+
+interface PaymentStoreAction {
+  getPayment: () => Promise<IPayment>;
+  getSimulations: () => Promise<ISimulation[]>;
+}
+
+const initialState: PaymentStoreState = {
+  payment: mockedPayment,
+  isLoadingPayment: false,
+  isLoadingSimulations: false,
+};
+
+export const usePaymentStore = create<PaymentStoreState & PaymentStoreAction>(
+  (set, get) => ({
+    ...initialState,
+    getPayment: async () => {
+      set({ isLoadingPayment: true });
+      try {
+        const result = await new Promise<IPayment>((resolve) =>
+          setTimeout(() => resolve(get().payment), 5000)
+        );
+        return result;
+      } catch (error) {
+        console.error("Error when trying to get payment", error);
+        throw error;
+      } finally {
+        set({ isLoadingPayment: false });
+      }
+    },
+    getSimulations: async () => {
+      set({ isLoadingSimulations: true });
+      try {
+        const result = await new Promise<ISimulation[]>((resolve) =>
+          setTimeout(() => resolve(get().payment.simulation), 5000)
+        );
+        return result;
+      } catch (error) {
+        console.error("Error when trying to get simulations", error);
+        throw error;
+      } finally {
+        set({ isLoadingSimulations: false });
+      }
+    },
+  })
+);
