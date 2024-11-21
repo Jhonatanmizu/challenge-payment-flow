89cd2933be30e4aab8afa50f052438c6e00841a0
jhonatanmizu <natanjesuss20@gmail.com>
2024-11-21 01:01:41 -0300

    :sparkles: feat: create payment success screen

--- src/app/(modules)/(common)/utils/format.ts
+++ src/app/(modules)/(common)/utils/format.ts
@@ -4,3 +4,13 @@ export const formatAmount = (amount: number) => {
     currency: "BRL",
   });
 };
+
+export const formatDateToLocaleBrazil = (date: Date) => {
+  if (!date) return "";
+
+  return date.toLocaleDateString("pt-BR", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+  });
+};
--- src/app/(modules)/(payment)/payment-success.tsx
+++ src/app/(modules)/(payment)/payment-success.tsx
@@ -1,11 +1,67 @@
-import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+// Components
+import { Box, CloseButton, Text } from "../(common)/components";
+import { Image, StyleSheet } from "react-native";
+// Translation
+import { useTranslation } from "react-i18next";
+// Utils
+import { formatDateToLocaleBrazil } from "../(common)/utils";
 
 const PaymentSuccess = () => {
+  const { t } = useTranslation();
+  const paymentDate = formatDateToLocaleBrazil(new Date(Date.now()));
+
+  const handleClose = () => {};
+
   return (
-    <View>
-      <Text>PaymentSuccess</Text>
-    </View>
+    <Box height="100%" backgroundColor="white" padding="lg">
+      <Box
+        flexDirection="row"
+        alignItems="center"
+        justifyContent="flex-end"
+        mb="2xl"
+      >
+        <CloseButton onClosePress={handleClose} />
+      </Box>
+      <Box mb="4xl">
+        <Text variant="headerBlack" fontWeight="700" color="main800">
+          {t("payment.pix_transfer_done_with")}
+        </Text>
+        <Text variant="headerBlack" fontWeight="700" color="main800">
+          {`${t("common.success")}!`}
+        </Text>
+      </Box>
+      <Box alignItems="center" rowGap="xs" justifyContent="center">
+        <Image
+          source={require("../../../assets/success_payment.png")}
+          resizeMode="cover"
+        />
+        <Text variant="titleBlack" fontWeight="700" color="main800">
+          {t("common.to")}
+        </Text>
+        <Text variant="titleBlack" fontWeight="bold" color="main800">
+          Maria da Silva Maria da Silva
+        </Text>
+        <Box flexDirection="row" columnGap="lg">
+          <Box alignItems="center">
+            <Text variant="titleBlack" fontWeight="bold" color="main800">
+              {t("common.value")}
+            </Text>
+            <Text variant="titleBlack" fontWeight="bold" color="main800">
+              R$100,00
+            </Text>
+          </Box>
+          <Box alignItems="center">
+            <Text variant="titleBlack" fontWeight="bold" color="main800">
+              {t("common.date")}
+            </Text>
+            <Text variant="titleBlack" fontWeight="bold" color="main800">
+              {paymentDate}
+            </Text>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
